02ac2eab5b6f64eadf952669c4a5748cfc66e898
Gagan Kumar <gagankumar8711@gmail.com>
2023-09-25 09:23:44 +0530

    Update head

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
         <>
             <title>ThirdEye</title>
             <meta
-                content="width=device-width, initial-scale=0.9"
+                content="width=device-width, initial-scale=1.0"
                 name="viewport"
             />
             <meta name="description" content="ThirdEye is a cutting-edge educational platform that seamlessly integrates assessment management, resource sharing, and personalized learning capabilities. Our platform serves as a central nexus, fostering collaboration among teachers, students, and parents, all while creating an immersive and interactive learning atmosphere." />
